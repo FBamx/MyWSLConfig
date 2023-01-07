f52e47339a8596ab6c4d0f0de1f5a35b8ac44122
elisii <495745954@qq.com>
2023-01-07 17:46:11 +0800

    add lsp and git shortcut

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -55,16 +55,28 @@ map("n", "gD", vim.lsp.buf.declaration)
 map("n", "gr", vim.lsp.buf.references)
 map("n", "gi", vim.lsp.buf.implementation)
 map("n", "<C-k>", vim.lsp.buf.signature_help)
-map("n", "<leader>ra", vim.lsp.buf.rename)
-map("n", "<leader>d", vim.diagnostic.open_float)
 map("n", "<M-n>", vim.diagnostic.goto_next)
 map("n", "<M-p>", vim.diagnostic.goto_prev)
+map("n", "<leader>ra", vim.lsp.buf.rename)
+map("n", "<leader>ca", vim.lsp.buf.code_action)
+map("n", "<leader>gr", vim.lsp.buf.references)
+map("n", "<leader>d", vim.diagnostic.open_float)
+map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder)
+map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder)
+map("n", "<leader>wl", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
 
+-- telescope
 map("n", "<leader>fb", ":Telescope file_browser<CR>", { silent = true })
+map("n", "<leader>fa", ":Telescope find_files follow=true no_ignore=true hidden=true<CR>")
+map("n", "<leader>fe", ":Telescope buffers<CR>")
+map("n", "<leader>fh", ":Telescope help_tags<CR>")
+map("n", "<leader>fo", ":Telescope oldfiles<CR>")
 map("n", "<leader>ft", ":TodoTelescope<CR>", { silent = true })
 map("n", "<leader>ff", ":Telescope find_files<CR>", { silent = true })
 map("n", "<leader>fw", ":Telescope live_grep<CR>", { silent = true })
 map("n", "<leader>fs", ":Telescope grep_string<CR>", { silent = true })
+map("n", "<leader>cm", ":Telescope git_commits<CR>")
+map("n", "<leader>gt", ":Telescope git_status<CR>")
 
 -- dap
 map("n", "<F1>", ":lua require 'dap'.toggle_breakpoint()<CR>")
@@ -73,6 +85,12 @@ map("n", "<F3>", ":lua require 'dap'.step_over()<CR>")
 map("n", "<F4>", ":lua require 'dap'.step_into()<CR>")
 map("n", "<F5>", ":lua require 'dap'.step_out()<CR>")
 map("n", "<F6>", ":lua require 'dapui'.close()<CR>")
+
+-- git
+map("n", "<leader>rh", ":lua require 'gitsigns'.reset_hunk()<CR>")
+map("n", "<leader>ph", ":lua require 'gitsigns'.preview_hunk()<CR>")
+map("n", "<leader>gb", ":lua package.loaded.gitsigns.blame_line()<CR>")
+map("n", "<leader>td", ":lua require 'gitsigns'.toggle_deleted()<CR>")
 
 -- nvim-tree
 map("n", "<C-n>", ":NvimTreeToggle<CR>")
