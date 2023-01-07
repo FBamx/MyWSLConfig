82c0de83137e7531ed1ca80323e6e9de28ef7730
elisii <495745954@qq.com>
2023-01-07 15:15:09 +0800

    fix some bug

--- lua/plugins/configs.lua
+++ lua/plugins/configs.lua
@@ -150,10 +150,7 @@ end
 
 configs.staline = function()
 
-  local ok, staline = pcall(require, "staline")
-  if not ok then
-    return
-  end
+  local staline = require("staline")
 
   staline.setup {
     defaults = {
@@ -525,11 +522,8 @@ end
 
 configs.noice = function()
 
-  local notify_ok, notify = pcall(require, "notify")
-  local noice_ok, noice = pcall(require, "noice")
-  if not (notify_ok and noice_ok) then
-    return
-  end
+  local notify = require("notify")
+  local noice = require("noice")
 
   notify.setup {
     background_colour = "#000000",
@@ -537,20 +531,18 @@ configs.noice = function()
 
   noice.setup {
     lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true,
       },
     },
-    -- you can enable a preset for easier configuration
     presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = false,
+      lsp_doc_border = false,
     },
   }
 
